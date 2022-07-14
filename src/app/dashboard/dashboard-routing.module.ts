60af837e2b17a918f0b0bc9d3c30c91288e44e81
Peyman <peymankhosravi.honor@gmail.com>
2022-07-14 13:23:22 +0430

    added side nav

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'market' },
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'market',
+        loadChildren: () =>
+          import('./market/market.module').then((m) => m.MarketModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
