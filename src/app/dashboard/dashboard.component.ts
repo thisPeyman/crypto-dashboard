fad7d0ddb1f8e38c3727f130cb1c075767a247e6
Peyman <peymankhosravi.honor@gmail.com>
2022-07-14 11:24:42 +0430

    added tailwind css and dashboard module

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
